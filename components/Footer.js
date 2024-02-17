8076a7f8a00c419bbaee030e933b92234e1fe540
lubuofficial <lilly9101@gmail.com>
2024-02-17 22:21:02 +0700

    deploy

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { FaFacebook } from 'react-icons/fa';
 import React from 'react';
-import { NavLink } from "react-router-dom";
 
 const FooterContainer = styled.footer`
   background-color: #444;
